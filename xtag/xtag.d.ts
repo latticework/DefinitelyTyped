64de17b86d370725e7e68485c1e9c6c0fcea82f9
Kenneth Brubaker <kennethbrubaker@hotmail.com>
2013-04-14 08:19:24 -0400

    CompilefFixed and finalized IXTagLifecycleRegisterOptions.created
    CompilefFixed by removing interface publics and putting optional member
    symbol after member symbol.
    Finalized IXTagLifecycleRegisterOptions.created signature.

--- xtag/xtag.d.ts
+++ xtag/xtag.d.ts
@@ -3,48 +3,51 @@
 // Definitions by: Kenneth Brubaker <https://github.com/kenbrubaker>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
+/// <reference path="lib.d.ts">
+
 interface IXTagLifecycleRegisterOptions {
     /** 
      * Fired once at the time a component is initially created or parsed. Allows you to modify the element before any 
      * other code is applied to it.
      */
-    public created()?;
+    created?(prototype: HTMLElement): void;
     /** 
      * Fired each time a component is inserted into the DOM. Allows you to do things like check the state or structure 
      * of the surrounding DOM tree or window scope and modify your component accordingly.
      */
-    public inserted()?;
+    inserted?();
     /** fired each time a element is removed from DOM */
-    public removed()?;
+    removed?();
     /** fired when attributes are set */
-    public attributeChanged()?;
+    attributeChanged?();
 }
+
 interface IXTagAccessor {
-    public get(): any;
-    public set(value: any)?: void; 
+    get(): any;
+    set?(value: any): void; 
 }
 interface IXTagRegisterOptions {
-    public lifecycle: IXTagLifecycleRegisterOptions;
+    lifecycle: IXTagLifecycleRegisterOptions;
 }
 
 interface IXTagStatic {
     /** Converts the given object into an array. */
-    public toArray(obj: Object): [];
+    toArray(obj: Object): any[];
     /** Returns a boolean that indicates if the element has the specified class. */
-    public hasClass(element: any, klass: string): bool;
+    hasClass(element: any, klass: string): bool;
     /** Adds a class to the element. */
-    public addClass(element: any, klass: string): any;
+    addClass(element: any, klass: string): any;
     /** Removes a class from the element. */
-    public removeClass(element: any, klass: string): any;
+    removeClass(element: any, klass: string): any;
     /** Adds the class if it doesn't exist on an element or removes the class if it exists. */
-    public toggleClass(element: any, klass: string): any;
+    toggleClass(element: any, klass: string): any;
     /** Returns a boolean that indicates if the given selector matches the element. */
-    public matchSelector(element: any, selector: string): bool;
+    matchSelector(element: any, selector: string): bool;
     /** Runs querySelector all on the given element and returns the results as an array. */
-    public query(element: any, selector: string): [];
+    query(element: any, selector: string): any[];
 
 
-    public register(name: string, options: any);
+    register(name: string, options: any);
 }
 
 declare var xtag: IXTagStatic;
